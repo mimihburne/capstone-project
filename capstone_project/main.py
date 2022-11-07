fb6ed71e754d94b0d168a25c4ab4e0266ea78ac0
Mimi Houlihan-Burne <mimihburne@gmail.com>
2022-11-07 14:55:26 +0000

    updated project 7/11 2

--- capstone_project/main.py
+++ capstone_project/main.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
 
+#read data file
 ACME_data = pd.read_csv('C:/Users/MimiHoulihan-Burne/OneDrive - JCW Resourcing/Data engineering/Capstone/test-data.csv')
 
 def find_column_types(df):
-    # identifies categorical, boolean and numerical values
+    #identify categorical, boolean and numerical values
     all_cols = list(df.columns)
     numerical_cols_temp = df._get_numeric_data().columns
     categorical_cols = list(set(all_cols) - set(numerical_cols_temp))
@@ -16,20 +17,21 @@ cat_cols, bool_cols, num_cols = find_column_types(ACME_data)
 
 ACME_data = ACME_data.dropna() #remove null rows to deal with potential future nulls
 
+#formatting data
 ACME_data['month'] = ACME_data['order_month'].str[:3]
 ACME_data['year'] = '20' + ACME_data['order_month'].str[-2:]
 ACME_data = ACME_data.drop(['order_month'], axis=1)
 ACME_data['uk_region'] = ACME_data['uk_region'].str[3:]
-
 ACME_data['age'] = abs(ACME_data['age']) #make all ages positive
 
-#validation scripts to check. know what format needs to be, run a check against data to check this
-#rest: check whether values are expected values- toothbrush, toys: value counts
+#use validation scripts to check required format is followed
+#check whether values are as expected, eg. sum of value counts of toothbrush, toys = total no.rows
 
 ACME_data.info()
-#print(ACME_data['product'].unique()) #check only toothbrush or toys
-#print(ACME_data['uk_region'].unique()) #no South data
-#print(ACME_data.dtypes) #one hot encode?
+#print(ACME_data['product'].unique()) ##check whether values are as expected; sum of value counts of toothbrush, toys = total no.rows
+#print(ACME_data['uk_region'].unique()) #shows no South data in current dataset, will skew findings
+#print(ACME_data.dtypes) #potential for one hot encoding
 print(ACME_data.value_counts())
 
+#save to a new file for analysis/visualisation
 ACME_data.to_csv('C:/Users/MimiHoulihan-Burne/OneDrive - JCW Resourcing/Data engineering/Capstone/cleaned_ACME_data.csv', index=False)
--- capstone_project/visualisations.py
+++ capstone_project/visualisations.py
@@ -3,7 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-#connect to server instead of having to import
+#style & font
+plt.style.use(['ggplot'])
+plt.rcParams['font.family'] = 'serif'
+
+#import SQL views- would be better to connect to server instead of having to import
 toys_region = pd.read_csv('C:/Users/MimiHoulihan-Burne/OneDrive - JCW Resourcing/Data engineering/Capstone/toys_region.csv')
 toothbrush_region = pd.read_csv('C:/Users/MimiHoulihan-Burne/OneDrive - JCW Resourcing/Data engineering/Capstone/toothbrush_region.csv')
 toys_agegroup = pd.read_csv('C:/Users/MimiHoulihan-Burne/OneDrive - JCW Resourcing/Data engineering/Capstone/toys_agegroup.csv')
@@ -22,6 +26,7 @@ combined_CPA = combined_CPA.fillna(0) #do something with null values that come f
 #print(combined_CPA.head())
 
 def overall_comparison_pie():
+    #pie chart displaying overall comparison of Toys and Toothbrushes bought
     explode = (0.2, 0)
     plt.pie(overall['Total_quantity'], labels=overall['Total_quantity'], explode=explode,  autopct='%.0f%%', shadow=True)
     plt.title('Total Sales of Toys and Toothbrushes')
@@ -30,6 +35,7 @@ def overall_comparison_pie():
     plt.show()
 
 def region_comparison_pies():
+    #pie chart displaying variation in sales of toothbrushes and toys across different regions
     fig, axs = plt.subplots(2)
     fig.suptitle('Region Comparisons for Toys and Toothbrush Sales')
     axs[0].pie(toothbrush_region['Total_quantity'], labels=toothbrush_region['Total_quantity'],  autopct='%.0f%%', shadow=True)
@@ -39,35 +45,44 @@ def region_comparison_pies():
     plt.show()
 
 def age_group_bar(): #axes, ordered
+    #combining both product types for plotting, with grouped seasons
+    toothbrush_agegroup['product'] = pd.Series(['toothbrush' for x in range(len(toothbrush_agegroup.index))])
+    toys_agegroup['product'] = pd.Series(['toys' for x in range(len(toys_agegroup.index))])
+    toys_toothbrush_agegroup = pd.concat([toothbrush_agegroup, toys_agegroup], axis=0)
+    ''' different approach:
+    #still not working as returning 1's for the y axis instead of 'total quantity'
+    age_groups = ['18 and Under', '18-24', '25-40', '40-60', '60 and Over']
+    sns.countplot(x='age_group', y='Total_quantity', hue='product', data = toys_toothbrush_agegroup, order=age_groups)
+    plt.show()'''
+
+    #bar chart displaying difference in sales of toothbrushes and toys across different age groups
     age_groups = ['18 and Under', '18-24', '25-40', '40-60', '60 and Over']
     plt.bar(toys_agegroup['age_group'], toys_agegroup['Total_quantity'])
     plt.bar(toothbrush_agegroup['age_group'], toothbrush_agegroup['Total_quantity'])
+    plt.title('Sales of Toys and Toothbrushes by Age Group')
+    plt.ylabel('Sales')
+    plt.xlabel('Age Groups')
+    #plt.legend(labels=toys_toothbrush_agegroup['product'], loc=1)
     plt.savefig('age_group_bar')
     plt.show()
-    #reorder
 
 def season_bar():
-    plt.bar(toys_season['season'], toys_season['Total_quantity'])
-    plt.bar(toothbrush_season['season'], toothbrush_season['Total_quantity'])
-    plt.savefig('season_bar')
-    plt.show()
-
-def countplots():
-    #combining both product types for plotting, with grouped ages/seasons
+    #combining both product types for plotting, with grouped seasons
     toothbrush_season['product'] = pd.Series(['toothbrush' for x in range(len(toothbrush_season.index))])
     toys_season['product'] = pd.Series(['toys' for x in range(len(toys_season.index))])
     toys_toothbrush_season = pd.concat([toothbrush_season, toys_season], axis=0)
 
-    toothbrush_agegroup['product'] = pd.Series(['toothbrush' for x in range(len(toothbrush_agegroup.index))])
-    toys_agegroup['product'] = pd.Series(['toys' for x in range(len(toys_agegroup.index))])
-    toys_toothbrush_agegroup = pd.concat([toothbrush_agegroup, toys_agegroup], axis=0)
-
-    #still not working as returning 1's for the y axis instead of 'total quantity'
-    age_groups = ['18 and Under', '18-24', '25-40', '40-60', '60 and Over']
-    sns.countplot(x='age_group', y='Total_quantity', hue='product', data = toys_toothbrush_agegroup, order=age_groups)
+    #bar chart displaying difference in sales of toothbrushes and toys across different seasons
+    plt.bar(toys_season['season'], toys_season['Total_quantity'])
+    plt.bar(toothbrush_season['season'], toothbrush_season['Total_quantity'])
+    plt.title('Sales of Toys and Toothbrushes by Season')
+    plt.ylabel('Sales')
+    plt.xlabel('Season')
+    #plt.legend(labels=toys_toothbrush_agegroup['product'], loc=1)
+    plt.savefig('season_bar')
     plt.show()
 
-# overall_comparison_pie()
-# region_comparison_pies()
-# age_group_bar()
-# season_bar()
+#overall_comparison_pie()
+#region_comparison_pies()
+age_group_bar()
+season_bar()
